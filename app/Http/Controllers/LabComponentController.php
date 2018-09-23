7390efb79dfed6e1165ac4675376a3705b209f97
Raj-Madheshia <rajsmadheshia@gmail.com>
2018-09-23 18:13:59 +0530

    Database

---
+++ app/Http/Controllers/LabComponentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Lab_Component;
+use Illuminate\Http\Request;
+
+class LabComponentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Lab_Component  $lab_Component
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lab_Component $lab_Component)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Lab_Component  $lab_Component
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Lab_Component $lab_Component)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Lab_Component  $lab_Component
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lab_Component $lab_Component)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Lab_Component  $lab_Component
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lab_Component $lab_Component)
+    {
+        //
+    }
+}
